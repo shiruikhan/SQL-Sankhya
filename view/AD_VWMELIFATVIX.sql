f11732d950cb19a6505362d74bc004edf16f0fb6
Silvio <svm.sistemas@hotmail.com>
2026-07-21 11:05:23 -0300

    feat: Create views AD_VWMELIFATVIX and AD_VWMELIFATVIX2 for invoice data retrieval

---
+++ view/AD_VWMELIFATVIX.sql
@@ -0,0 +1,46 @@
+    CREATE OR REPLACE VIEW AD_VWMELIFATVIX AS SELECT
+        0 AS ID					,
+        
+				C.NUMNOTA				,
+        
+				C.NUNOTA				,
+        
+				C.SERIENOTA				,
+        
+				C.DTFATUR				,
+        
+				C.STATUSNFE				,
+        
+				C.AD_PEDIDOMKTPLACE		,
+        
+				c.AD_MELISHIPID AS AD_SHIPID			,
+        
+				C.CHAVENFE				,
+        
+				C.CODVEND				,
+        
+				C.CODEMP				,
+        
+				N.XMLENVCLI	AS NOTAXML
+FROM TGFCAB C 
+    INNER JOIN
+        TGFNFE N 
+            ON C.NUNOTA = N.NUNOTA
+WHERE	C.TIPMOV	= 'V' 
+            AND C.CODVEND	= 5 
+			AND C.CODEMP = 1
+            AND C.AD_PEDIDOMKTPLACE IS NOT NULL
+AND C.STATUSNFE = 'A' 
+            AND C.DTFATUR >= SYSDATE-7
+AND C.NUNOTA NOT IN (
+                SELECT
+                    CODATA 
+                     
+            FROM
+                TSIATA
+                     
+            WHERE
+                DESCRICAO  like '%Etiqueta%' 
+                     
+                AND TIPO ='N' 
+        );
---
+++ view/AD_VWMELIFATVIX2.sql
@@ -0,0 +1,46 @@
+    CREATE VIEW AD_VWMELIFATVIX2 AS SELECT
+        0 AS ID					,
+        
+				C.NUMNOTA				,
+        
+				C.NUNOTA				,
+        
+				C.SERIENOTA				,
+        
+				C.DTFATUR				,
+        
+				C.STATUSNFE				,
+        
+				C.AD_PEDIDOMKTPLACE		,
+        
+				c.AD_MELISHIPID AS AD_SHIPID			,
+        
+				C.CHAVENFE				,
+        
+				C.CODVEND				,
+        
+				C.CODEMP				,
+        
+				N.XMLENVCLI	AS NOTAXML
+FROM TGFCAB C 
+    INNER JOIN
+        TGFNFE N 
+            ON C.NUNOTA = N.NUNOTA
+WHERE	C.TIPMOV	= 'V' 
+            AND C.CODVEND	= 5 
+			AND C.CODEMP = 2
+            AND C.AD_PEDIDOMKTPLACE IS NOT NULL
+AND C.STATUSNFE = 'A' 
+            AND TO_DATE(C.DTFATUR) >= TO_DATE(SYSDATE-7)
+AND C.NUNOTA NOT IN (
+                SELECT
+                    CODATA 
+                     
+            FROM
+                TSIATA
+                     
+            WHERE
+                DESCRICAO  like '%Etiqueta%' 
+                     
+                AND TIPO ='N' 
+        )
